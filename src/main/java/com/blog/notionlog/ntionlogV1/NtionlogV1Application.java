6aa0c5c9050db73a23bd94c79433f13eb3d4dc2f
jongGuk <qorwhdr@naver.com>
2024-05-11 20:24:03 +0900

    push project files

---
+++ src/main/java/com/blog/notionlog/ntionlogV1/NtionlogV1Application.java
@@ -0,0 +1,13 @@
+package com.blog.notionlog.ntionlogV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtionlogV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtionlogV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/notionlog/ntionlogV1/NtionlogV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.notionlog.ntionlogV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtionlogV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
